7a26643e493da358d0829899ae329c7f555f7fa5
Forest Darling <fdarling@gmail.com>
2025-03-02 16:49:50 -0600

    supressed unused variable warning

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -8,12 +8,14 @@ void GameObject::preSolve(b2Contact *contact, const b2Manifold *oldManifold, b2F
 {
     (void)contact;
     (void)oldManifold;
+    (void)other;
 }
 
 void GameObject::postSolve(b2Contact *contact, const b2ContactImpulse *impulse, b2Fixture *other)
 {
     (void)contact;
     (void)impulse;
+    (void)other;
 }
 
 void GameObject::beginContact(b2Contact *contact, b2Fixture *other)
